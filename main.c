51852cd29744d507b3b78fff7becd1cfa7a98569
Furkan Kuyumcu <kuyumcufurkan05@gmail.com>
2024-05-21 20:23:01 +0300

    First Commit!

---
+++ main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int** createMatrix(int row_size, int column_size);
+
+int main() {
+
+    int rows = 5;
+    int cols = 5;
+    int **matrix = createMatrix(rows, cols);
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            matrix[i][j] = i * j;
+        }
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d\t", matrix[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+int** createMatrix(int row_size, int column_size) {
+    int **matrix = (int**) malloc(row_size * sizeof(int*));
+    if (matrix == NULL) {
+        printf("Memory allocation failed.\n");
+        exit(1);
+    }
+
+    for (int i = 0; i < row_size; i++) {
+        matrix[i] = (int*)malloc(column_size * sizeof(int));
+        if (matrix[i] == NULL) {
+            printf("Memory allocation failed.\n");
+            exit(1);
+        }
+    }
+    
+    return matrix;
+}
